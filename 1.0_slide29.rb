aee9b70b3095a3b14a596d48481a2af0643661c6
Edric Emmanuel Sia <edric.sia@gmail.com>
2021-10-22 20:40:26 +0800

    Added new exercises

--- 1.0_slide29.rb
+++ 1.0_slide29.rb
@@ -1,19 +1,21 @@
-# 1 
+# 1 --------------------- Checks if array includes number -------------------- #
 arr = [1,3,5,7,9,11]
 number = 3
 
 puts "#1 Does the array #{arr} include the number #{number}?" , arr.include?(number)
 
-# 2
+
+
+# 2 ------- Checks if number is between 0 and 50 or between 51 and 100 ------- #
 puts "#2 Give me a number between 0 and 100"
 
 while number = gets.chomp.to_i
-    if number > 0 && number < 100
-        if number > 0 && number <= 50
-            puts "#{number} is between 0 and 51"
+    if number >= 0 && number <= 100
+        if number >= 0 && number <= 50
+            puts "#{number} is between 0 and 50"
             break
         else 
-            puts "#{number} is between 50 and 100"
+            puts "#{number} is between 51 and 100"
         end
     break
     else
@@ -21,7 +23,9 @@ while number = gets.chomp.to_i
     end
 end
 
-# 3
+
+
+# 3 --------------------------- Get input from user -------------------------- #
 puts "#3 Type anything to start. Type 'STOP' (all caps) to stop."
 
 while input = gets.chomp
@@ -35,8 +39,10 @@ while input = gets.chomp
     end
 end
 
-#4 
-puts "#4 Check if numbers in an array are divisible by 2"
+
+
+# 4 ------------- Check which numbers in array are divisible by 2 ------------ #
+puts "#4 Check which numbers in an array are divisible by 2"
 arr2 = [6, 3, 1, 8, 4, 2, 10, 65, 102]
 arr_even = []
 
